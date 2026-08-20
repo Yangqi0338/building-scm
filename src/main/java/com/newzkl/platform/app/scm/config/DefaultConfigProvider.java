b0ba5b432c6255d42bed358fe10b132a24e965c0
KC <kchange0915@gmail.com>
2026-08-20 10:47:44 +0800

    1.0

--- src/main/java/com/newzkl/platform/app/scm/config/DefaultConfigProvider.java
+++ src/main/java/com/newzkl/platform/app/scm/config/DefaultConfigProvider.java
@@ -2,7 +2,7 @@ package com.newzkl.platform.app.scm.config;
 
 import com.newzkl.platform.base.common.ddd.application.spi.IdentityImpl;
 import com.newzkl.platform.base.common.ddd.application.spi.demo.IdentityConfigExt;
-import com.newzkl.platform.base.common.ddd.model.enums.user.RoleEnum;
+import com.newzkl.platform.base.common.ddd.model.enums.account.AccountEnum;
 
 /**
  * 身份配置提供-平台默认实现(演示)。
@@ -12,7 +12,7 @@ import com.newzkl.platform.base.common.ddd.model.enums.user.RoleEnum;
  *
  * @author KC
  */
-@IdentityImpl({RoleEnum.CompanyRole.PLATFORM, RoleEnum.CompanyRole.EMP})
+@IdentityImpl({AccountEnum.Identity.PLATFORM, AccountEnum.Identity.EMP})
 public class DefaultConfigProvider implements IdentityConfigExt {
 
     @Override
